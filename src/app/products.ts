72e592ff1c3679f8f0820a6d0b77bb4bef92bd83
Oney-Dude <oneydude@gmail.com>
2019-11-15 10:58:07 -0600

    3 files modified

--- src/app/products.ts
+++ src/app/products.ts
@@ -1,17 +1,17 @@
 export const products = [
   {
     name: 'Lubbock',
-    threatLVL: 799,
+    threatLVL: 1,
     description: 'No one would notice its gone...'
   },
   {
     name: 'The Moon',
-    threatLVL: 699,
+    threatLVL: 69,
     description: 'Aliens could be hidden beneath the surface.'
   },
   {
     name: 'Brians Apartment',
-    threatLVL: 299,
+    threatLVL: 999,
     description: 'A Really hot guy lives here.'
   }
 ];
